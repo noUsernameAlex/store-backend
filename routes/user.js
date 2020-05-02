7769f77375e72addde33a82018110ff560d1e497
AlexFly <hao.alex.fly@gmail.com>
2020-05-01 23:57:32 -0500

    add admin in database

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
+const Admin = require('../models/admin.model');
 const bcrypt = require("bcrypt");
 
 router.route('/').post((req, res) => {
   const {email, password} = req.body;
+  let userIsAdmin = false;
   User.findOne({ email : email }).then(user => {
     // Check if user exists
     console.log(`${email} is trying to log in!`);
@@ -15,14 +17,21 @@ router.route('/').post((req, res) => {
       if (isMatch) {
         // User matched
         // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name
-        };
-        res.json({
-          success: true,
+        console.log("111");
+        Admin.findOne({email : email}).then(adminResult => {
+          if (adminResult) {
+            console.log("222");
+            res.json({
+              isAdmin : true,
+              success: true,
+            });
+          } else {
+            res.json({
+              success: true,
+            });
+            console.log(`${email} logged in successfully!`);
+          }
         });
-        console.log(`${email} logged in successfully!`);
       } else {
         res.status(400).json('password incorrect');
       }
@@ -34,6 +43,34 @@ router.route('/all').get((req, res) => {
     User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('error:' + err));
+});
+
+router.route('/addAdmin').post((req, res) => {
+  const {email, password} = req.body;
+  console.log('adding admin : email' + email + " password : " + password);
+
+  Admin.findOne({ email: email }).then(user => {
+      if (user) {
+        return res.status(400).json({ email: "Email already exists" });
+      } else {
+        const newAdmin = new Admin({
+          email: email,
+          password: password,
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newAdmin.password, salt, (err, hash) => {
+            if (err) {
+              throw err;
+            }
+            newAdmin.password = hash;
+            newAdmin
+              .save()
+              .then(newAdmin => {res.json(newAdmin)})
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
 });
 
 router.route('/add').post((req, res) => {
